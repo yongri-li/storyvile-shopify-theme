15c1d435fc5d2fba2d18148406ec75da559a1497
yongri-li <yongri.liii+1@gmail.com>
2020-08-31 17:12:48 -0300

    pulling changes from live theme

---
+++ assets/zendesk-embeddable.js
@@ -0,0 +1,1 @@
+var script = document.createElement('script');script.type = 'text/javascript';script.id = 'ze-snippet';script.src = 'https://static.zdassets.com/ekr/snippet.js?key=865cf939-eddc-4fb0-bf56-33941fdf5ec6';document.getElementsByTagName('head')[0].appendChild(script);
